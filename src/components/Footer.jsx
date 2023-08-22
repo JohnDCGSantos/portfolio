4210fa767d47658a5164274f897a1fbb57228358
JohnDCGSantos <jdcg.santos@gmail.com>
2023-08-22 18:31:24 +0100

    pppp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import '../Footer.css'
 import { useEffect, useState } from 'react'
 
-import { FaHome, FaReact, FaNodeJs, FaCss3Alt, FaBootstrap } from 'react-icons/fa'
+import { FaHome, FaReact, FaNodeJs, FaCss3Alt, FaBootstrap } from 'react-icons/Fa'
 
 import { BiLogoMongodb } from 'react-icons/Bi'
 import { TbBrandJavascript } from 'react-icons/Tb'
