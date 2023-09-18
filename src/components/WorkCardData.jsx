657bcb9fae0f2aed10c1f6d7b41fa60342df95bd
JohnDCGSantos <jdcg.santos@gmail.com>
2023-09-18 18:39:05 +0100

    updated

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -1,4 +1,5 @@
 import CosmicBlasters from '../CosmicBlasters.png'
+import spaceB from '../spaceB.png'
 import project2 from '../project2.png'
 import project3 from '../project3.png'
 const WorkCardData = [
@@ -21,11 +22,23 @@ const WorkCardData = [
   },
   {
     id: 3,
+    imgsrc: spaceB,
+
+    title: 'Space Bubble',
+
+    text: ' "Embark on an epic adventure with Space Bubble, a mesmerizing game where the worlds of DOM manipulation, Object-Oriented Programming (OOP), canvas rendering, and GSAP animation converge. Command your spacebubble through a canvas-rendered galaxy, employing OOP principles to navigate and interact with a dynamic DOM-driven interface. Engage with a variety of JavaScript-driven features, including colorful enemy encounters, power-up mechanics, and real-time scoring, creating a visually stunning spectacle. Prepare for an interstellar journey that showcases the magic of JavaScript gaming like never before!"',
+
+    skills: ' -JavaScript · HTML · CSS · Canvas · gsap-',
+
+    link: 'https://johndcgsantos.github.io/Space-Bubble/',
+  },
+  {
+    id: 4,
     imgsrc: CosmicBlasters,
 
     title: 'Cosmic Blasters',
 
-    text: '"Cosmic Blasters" is a browser-based shooting game that draws inspiration from retro space-ship shooters. Developed using fundamental technologies such as JavaScript, HTML, and CSS, the game operates through DOM manipulation and object-oriented programming principles.',
+    text: '"Cosmic Blasters" is my first ever project done 3 weeks after begining my web development journey; is a browser-based shooting game that draws inspiration from retro space-ship shooters. Developed using fundamental technologies such as JavaScript, HTML, and CSS, the game operates through DOM manipulation and object-oriented programming principles.',
 
     skills: ' -JavaScript · HTML · CSS · Bootstrap-',
 
