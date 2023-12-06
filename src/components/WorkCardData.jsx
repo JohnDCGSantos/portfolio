47a23be3568cf6499b29fe26ac99f3e5a91cd0f6
JohnDCGSantos <jdcg.santos@gmail.com>
2023-12-06 23:15:31 +0000

    add new project

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -2,9 +2,22 @@ import CosmicBlasters from '../CosmicBlasters.png'
 import spaceB from '../spaceB.png'
 import project2 from '../project2.png'
 import project3 from '../project3.png'
+import DocPort from '../DocPort.png'
 const WorkCardData = [
   {
     id: 1,
+    imgsrc: DocPort,
+
+    title: 'Gestão de Urgências Hospitalares (Emergency Medshift Scheduler)',
+
+    text: ' "I designed this application to streamline the administration of medical rosters, custom-tailored to comply with the regulations of a Portuguese hospital. Behind the scenes, I utilized JavaScript for server-side logic, with Express and Node.js, Bcrypt for secure password management, and MongoDB as a robust database. On the client side, EJS and Views were employed to create a user-friendly experience, complemented by CSS for aesthetic appeal. This application is purpose-built to simplify the generation of medical rosters while aligning with the unique protocols of a specific hospital. It has been a fulfilling journey, and I am always eager to learn and refine my skills. Moreover, I am committed to adapting this application to the specific requirements of other hospitals should the need arise."',
+
+    skills: ' -Express · Node.js · Bcrypt · EJS · MongoDB · JavaScript · HTML · CSS & Bootstrap-',
+
+    link: 'https://emergency-medshift-scheduler.adaptable.app/',
+  },
+  {
+    id: 2,
     imgsrc: spaceB,
 
     title: 'Space Bubble',
@@ -16,7 +29,7 @@ const WorkCardData = [
     link: 'https://johndcgsantos.github.io/Space-Bubble/',
   },
   {
-    id: 2,
+    id: 3,
     imgsrc: project3,
     title: 'LinkdIron',
     text: 'LinkdIron is a networking platform that enables users to connect, share posts about events, job opportunities, and valuable insights. Users can create, read, update, and delete posts containing text, images, and videos. They can also search for and follow/unfollow other users. The client utilizes React, while the server and REST APIs are built with Express.js.',
@@ -25,7 +38,7 @@ const WorkCardData = [
     link: 'https://linkdiron.netlify.app/Signup',
   },
   {
-    id: 3,
+    id: 4,
     imgsrc: project2,
     title: 'Fitness App',
     text: 'Built on Express.js and EJS templating, FitnessApp empowers users to create personalized exercise plans with 400+ exercises. Craft multiple plans with instructions and images, and seamlessly manage updates or deletions.',
@@ -34,7 +47,7 @@ const WorkCardData = [
   },
 
   {
-    id: 4,
+    id: 5,
     imgsrc: CosmicBlasters,
 
     title: 'Cosmic Blasters',
