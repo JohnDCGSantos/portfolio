9eeae984f8bf79c4089ad1d34357948bdcb54ca8
JohnDCGSantos <jdcg.santos@gmail.com>
2023-12-06 20:47:11 +0000

    adding

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -5,23 +5,6 @@ import project3 from '../project3.png'
 const WorkCardData = [
   {
     id: 1,
-    imgsrc: project3,
-    title: 'LinkdIron',
-    text: 'LinkdIron is a networking platform that enables users to connect, share posts about events, job opportunities, and valuable insights. Users can create, read, update, and delete posts containing text, images, and videos. They can also search for and follow/unfollow other users. The client utilizes React, while the server and REST APIs are built with Express.js.',
-    skills:
-      '-React · Rest APIs · Express · Node.js · MongoDB · JavaScript · HTML · CSS & Bootstrap-',
-    link: 'https://linkdiron.netlify.app/Signup',
-  },
-  {
-    id: 2,
-    imgsrc: project2,
-    title: 'Fitness App',
-    text: 'Built on Express.js and EJS templating, FitnessApp empowers users to create personalized exercise plans with 400+ exercises. Craft multiple plans with instructions and images, and seamlessly manage updates or deletions.',
-    skills: '    -Express · Node.js · EJS · MongoDB · JavaScript · HTML · CSS & Bootstrap-',
-    link: 'https://fitnessgo.adaptable.app',
-  },
-  {
-    id: 3,
     imgsrc: spaceB,
 
     title: 'Space Bubble',
@@ -32,6 +15,24 @@ const WorkCardData = [
 
     link: 'https://johndcgsantos.github.io/Space-Bubble/',
   },
+  {
+    id: 2,
+    imgsrc: project3,
+    title: 'LinkdIron',
+    text: 'LinkdIron is a networking platform that enables users to connect, share posts about events, job opportunities, and valuable insights. Users can create, read, update, and delete posts containing text, images, and videos. They can also search for and follow/unfollow other users. The client utilizes React, while the server and REST APIs are built with Express.js.',
+    skills:
+      '-React · Rest APIs · Express · Node.js · MongoDB · JavaScript · HTML · CSS & Bootstrap-',
+    link: 'https://linkdiron.netlify.app/Signup',
+  },
+  {
+    id: 3,
+    imgsrc: project2,
+    title: 'Fitness App',
+    text: 'Built on Express.js and EJS templating, FitnessApp empowers users to create personalized exercise plans with 400+ exercises. Craft multiple plans with instructions and images, and seamlessly manage updates or deletions.',
+    skills: '    -Express · Node.js · EJS · MongoDB · JavaScript · HTML · CSS & Bootstrap-',
+    link: 'https://fitnessgo.adaptable.app',
+  },
+
   {
     id: 4,
     imgsrc: CosmicBlasters,
